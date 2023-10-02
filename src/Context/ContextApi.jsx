06669f63232670aa17c0f814449a55d75f2e57f6
sahil <91470287+sahilthapa001@users.noreply.github.com>
2023-10-02 12:30:56 +0545

    did major changes where applying slice and splice instead of map or filter

--- src/Context/ContextApi.jsx
+++ src/Context/ContextApi.jsx
@@ -5,20 +5,33 @@ export const TodoContext = createContext();
 const todoReducer = (state, action) => {
 	switch (action.type) {
 		case "ADD_TODO":
-			return [
-				...state,
-				{ id: Date.now(), text: action.payload, completed: false },
-			];
-		case "TOGGLE_TODO":
-			return state.map((todo) =>
-				todo.id === action.payload
-					? { ...todo, completed: !todo.completed }
-					: todo
-			);
+			const newTodo = {
+				id: Date.now(),
+				text: action.payload,
+				completed: false,
+			};
+			return state.slice().concat(newTodo);
 
+		case "TOGGLE_TODO":
+			const indexToToggle = state.findIndex(
+				(todo) => todo.id === action.payload
+			);
+			if (indexToToggle !== -1) {
+				const newState = state.slice();
+				const todoToToggle = newState[indexToToggle];
+				todoToToggle.completed = !todoToToggle.completed;
+				return newState;
+			}
 		case "DELETE_TODO":
-			return state.filter((todo) => todo.id !== action.payload);
-            
+			const indexToDelete = state.findIndex(
+				(todo) => todo.id === action.payload
+			);
+			console.log(indexToDelete);
+			if (indexToDelete !== -1) {
+				const newState = state.slice();
+				newState.splice(indexToDelete, 1);
+				return newState;
+			}
 		case "SEARCH_TODO":
 			return state.filter((todo) => todo.text.includes(action.payload));
 
@@ -29,8 +42,6 @@ const todoReducer = (state, action) => {
 
 export const TodoContextProvider = ({ children }) => {
 	const [todos, dispatch] = useReducer(todoReducer, []);
-	const [task, setTask] = useState("");
-	const TodoContextValue = { task, setTask };
 	return (
 		<TodoContext.Provider value={{ todos, dispatch }}>
 			{children}
